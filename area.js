0a8c2354915ae70e7519504dedbb85b36655badd
elsie94 <elsie9421@gmail.com>
2025-08-07 02:25:59 -0700

    first commit

---
+++ area.js
@@ -0,0 +1,8 @@
+//Function to calculate the area of a circle
+const PI = 22/7;
+function calculateAreaOfACircle(r){
+return PI*r*r;
+
+}
+console.log(calculateAreaOfACircle(7));
+console.log("The Area of a Circle is: " + calculateAreaOfACircle(7) +" meters squared");
